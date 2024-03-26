46dd5187c52bf0d21b9a79d5e5b950c85c43f5c2
tkolsrud <thomaskolsrud@gmail.com>
2024-03-26 14:31:22 -0700

    update both url files register admin page models

--- mvc_mvps/mvc_mvps_app/admin.py
+++ mvc_mvps/mvc_mvps_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Conference, Team, Player
+admin.site.register(Conference)
+admin.site.register(Team)
+admin.site.register(Player)
---
+++ mvc_mvps/mvc_mvps_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from rest_framework.routers import DefaultRouter
+
+urlspatters = [
+    path('conferences/', views.ConferenceList.as_view(), name='conference_list'),
+    path('conferences/<int:pk>', views.ConferenceDetail.as_view(), name='conference_detail'),
+    path('teams/', views.TeamList.as_view(), name='team_list'),
+    path('teams/<int:pk>', views.TeamDetail.as_view(), name='team_detail'),
+    path('players/', views.PlayerList.as_view(), name='player_list'),
+    path('players/<int:pk>', views.PlayerDetail.as_view(), name='player_detail')
+]
