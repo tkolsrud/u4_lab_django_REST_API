89a9e54da02bffcd86a6c93ef553228bd94e2072
tkolsrud <thomaskolsrud@gmail.com>
2024-03-26 14:23:01 -0700

    add views

--- mvc_mvps/mvc_mvps_app/views.py
+++ mvc_mvps/mvc_mvps_app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import ConferenceSerializer, TeamSerializer, PlayerSerializer
+from .models import Conference, Team, Player
 # Create your views here.
+
+class ConferenceList(generics.ListCreateAPIView):
+    queryset = Conference.objects.all()
+    serializer_class = ConferenceSerializer
+
+class ConferenceDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Conference.objects.all()
+    serializer_class = ConferenceSerializer
+
+class TeamList(generics.ListCreateAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class TeamDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class PlayerList(generics.ListCreateAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+class PlayerDetail(generics.RetrieveUpdateDestoryAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+
